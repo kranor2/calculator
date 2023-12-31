c28a5b6f4168a51d35e3ca47bb23cb0fe8d07c07
Rhett North <retsrawk@gmail.com>
2023-12-30 21:26:56 -0700

    Add function for number buttons
    This function includes a click-based event listener for all number
    buttons, provisions for if numOne or numTwo becomes too big or too long
    for the screen, and unlike my previous addition, ensures the clarity of
    what should be the textContent of numOne and numTwo.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let display = document.querySelector(".main-screen");
 let numOne = "";
 let numTwo = "";
 let operator = "";
+let output = "";
 let solution = 0;
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const numButtons = document.querySelector(".num-button");
@@ -24,3 +25,30 @@ function operate() {
     numTwo = "";
 };
 
+numButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (output === "") {
+            numOne += button.value;
+            if (numOne.length >= 12) {
+                currentText.textContent = "Too big. Try again."
+                numOne = "";
+            } else {
+                currentText.textContent = numOne;
+            }
+        } else if (output.charAt(output.length-2) === "=") {
+            numOne = "";
+            numOne += button.value;
+            currentText.textContent = numOne;
+            output = "";
+            upperScreenText.textContent = output;
+        } else {
+            numTwo += button.value;
+            if (numTwo.length >= 12) {
+                currentText.textContent = "Too bit. Try again."
+                numTwo = "";
+            } else {
+                currentText.textContent = numTwo;
+            }
+        }
+    })
+});
